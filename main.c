ca2ec55a063231676f5feab2158133fa13d010e3
Jasper Arneberg <C16Jasper.Arneberg@usafa.edu>
2014-10-21 22:08:50 -0600

    two bouncing balls at once

--- main.c
+++ main.c
@@ -14,12 +14,15 @@ void main(void) {
     	initNokia();
     	clearDisplay();
 
-    ball myBall = createBall(XPOS_0,YPOS_0,XVEL_0,YVEL_0,4); //creates ball at (0,0) with velocity (1,1) and radius 4
+    ball myBall = createBall(XPOS_0,YPOS_0,XVEL_0,YVEL_0,4); //creates ball at (1,1) with velocity (1,1) and radius 4
+    ball myBall2 = createBall(3,4,-1,1,4);
 
     while(1) {							//infinte loop
     	myBall = moveBall(myBall);		//put breakpoint here to observe ball movement
+    	myBall2 = moveBall(myBall2);
     	clearDisplay();
        	drawBlock(myBall.xpos,myBall.ypos,BLACK);
+       	drawBlock(myBall2.xpos,myBall2.ypos,BLACK);
 
     	//wait for a certain period of time
     	long i = 500000;
